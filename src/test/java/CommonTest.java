f4de9452fea6413d04f8cd390a9dae56aaca45ad
Maria Bulgakova <bulgakova.m@gmail.com>
2022-11-09 22:46:13 +0500

    Добавлен slf4j logger

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -1,10 +1,13 @@
 import Constants.Config;
+import PageObjects.MainPage;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
-import PageObjects.MainPage;
 
 public class CommonTest {
     protected static WebDriver driver;
@@ -12,11 +15,14 @@ public class CommonTest {
     // Главная страница
     MainPage mPage;
 
+    final Logger logger = LoggerFactory.getLogger(CommonTest.class);
+
     @Before
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(Config.MAIN_URI);
+        logger.info("Get url " + Config.MAIN_URI);
         mPage = new MainPage(driver);
     }
 
